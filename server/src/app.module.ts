ca24302108a17296f03446d3a4ea6a197d70584c
VIREAK KEOSOKVIBOL <vireakkeosokvibol@softlibrary.org>
2021-10-27 16:11:14 +0700

    Reconfig github action and add static content support to NestJS

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,12 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    })
+  ],
 })
 export class AppModule {}
