066df88d5dac7f902722f0b717b19d0dff5280c7
VIREAK KEOSOKVIBOL <vireakkeosokvibol@softlibrary.org>
2021-11-01 21:55:19 +0700

    Add support for GraphQL (Not complete)

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,20 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { GraphQLModule } from '@nestjs/graphql';
+import { AppResolver } from './app.resolver';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
-    })
+    }),
+    GraphQLModule.forRoot({
+      playground: true,
+      debug: false,
+      autoSchemaFile: join(__dirname, 'src/schema.gql'),
+    }),
   ],
+  providers: [AppResolver],
 })
-export class AppModule {}
+export class AppModule { }
